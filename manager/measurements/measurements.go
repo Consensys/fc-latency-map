09b9e8220a19d62696cd370030123e511b254f09
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-09-16 09:39:46 +0100

    rename comments

--- manager/measurements/measurements.go
+++ manager/measurements/measurements.go
@@ -8,21 +8,21 @@ import (
 type MeasurementService interface {
 	RipeCreateMeasurements()
 
-	// CreatePingByType  creates a Ping MeasurementResults for a specific type
+	// RipeCreatePingWithProbes  creates a Ping MeasurementResults for a specific type
 	RipeCreatePingWithProbes(miners []*models.Miner, probeIDs string) (*atlas.MeasurementRequest, *atlas.MeasurementResp, error)
-
+	// RipeCreatePing
 	RipeCreatePing(miners []*models.Miner, probes []atlas.ProbeSet) (*atlas.MeasurementRequest, *atlas.MeasurementResp, error)
 
-	// GetMeasurement returns MeasurementResults from ID
+	// RipeGetMeasurement returns MeasurementResults from ID
 	RipeGetMeasurement(id int) (*atlas.Measurement, error)
 
 	// GetMeasurementResult
 	RipeGetMeasurementResult(id int, start int) ([]atlas.MeasurementResult, error)
 
-	// GetMeasures load RIPE MeasurementResults
+	// RipeGetMeasures load RIPE MeasurementResults
 	RipeGetMeasures()
 
-	// ExportData from db to json file
+	// dbExportData from db to json file
 	dbExportData(fn string)
 
 	dbCreate(measurements []*models.Measurement)
