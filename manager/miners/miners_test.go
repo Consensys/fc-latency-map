f0f167ff21ab4f09bdea0fdb27c215655833acc5
Samir <samir.saidali@consensys.net>
2021-09-24 15:38:00 +0200

    feat: improve unit tests

--- manager/miners/miners_test.go
+++ manager/miners/miners_test.go
@@ -3,6 +3,7 @@ package miners
 import (
 	"testing"
 
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/ConsenSys/fc-latency-map/manager/config"
@@ -24,4 +25,44 @@ func Test_GetAllMiners(t *testing.T) {
 
 	miners := srv.GetAllMiners()
 	assert.NotNil(t, miners)
+}
+
+func Test_ParseMiners(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dummyOffset := uint(42)
+	dummyBlockHeight := int64(42)
+	dummyVerifiedDeals := make([]fmgr.VerifiedDeal, 0)
+	mockConfig := config.NewMockConfig()
+	mockDbMgr := db.NewMockDatabaseMgr()
+	mockFMgr := fmgr.NewMockFilecoinMgr(ctrl)
+	mockGMgr := geomgr.NewMockGeoMgr(ctrl)
+	srv := NewMinerServiceImpl(mockConfig, mockDbMgr, mockFMgr, mockGMgr)
+
+	mockFMgr.EXPECT().GetBlockHeight().Return(abi.ChainEpoch(dummyBlockHeight), nil)
+	mockFMgr.EXPECT().GetVerifiedDealsByBlockRange(abi.ChainEpoch(dummyBlockHeight), dummyOffset).Return(dummyVerifiedDeals, nil)
+
+	miners := srv.ParseMiners(dummyOffset)
+	assert.NotNil(t, miners)
+	assert.Empty(t, miners)
+}
+
+func Test_ParseMinersByBlockHeight(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dummyBlockHeight := int64(42)
+	dummyVerifiedDeals := make([]fmgr.VerifiedDeal, 0)
+	mockConfig := config.NewMockConfig()
+	mockDbMgr := db.NewMockDatabaseMgr()
+	mockFMgr := fmgr.NewMockFilecoinMgr(ctrl)
+	mockGMgr := geomgr.NewMockGeoMgr(ctrl)
+	srv := NewMinerServiceImpl(mockConfig, mockDbMgr, mockFMgr, mockGMgr)
+
+	mockFMgr.EXPECT().GetVerifiedDealsByBlockHeight(abi.ChainEpoch(dummyBlockHeight)).Return(dummyVerifiedDeals, nil)
+
+	miners := srv.ParseMinersByBlockHeight(dummyBlockHeight)
+	assert.NotNil(t, miners)
+	assert.Empty(t, miners)
 }
