01a2dc985f74797a1aa58231edd342b1f23cb27c
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-10-01 14:51:11 +0100

    remove json struct

--- manager/models/probes.go
+++ manager/models/probes.go
@@ -9,7 +9,7 @@ type Probe struct {
 	ProbeID     int      `gorm:"column:probe_id;uniqueIndex"`
 	CountryCode string   `gorm:"column:country_code"`
 	IataCode    string   `gorm:"foreignKey:iata_code"`
-	Location    Location `gorm:"foreignkey:IataCode;references:iata_code"`
+	Location    Location `gorm:"foreignkey:IataCode;references:iata_code" json:"-"`
 	Latitude    float64  `gorm:"column:latitude" json:"latitude,omitempty"`
 	Longitude   float64  `gorm:"column:longitude" json:"longitude,omitempty"`
 }
