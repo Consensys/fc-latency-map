d6c9dd04c65181796f23a907b16f108aa75f9d9c
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-10-04 08:57:01 +0100

    data dump

--- manager/data/schema.sql
+++ manager/data/schema.sql
@@ -1,116 +1,17 @@
-create table locations
-(
-    id integer
-        primary key,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
-    country text,
-    iata_code text,
-    latitude real,
-    longitude real
-);
-
-create index idx_locations_deleted_at
-	on locations (deleted_at);
-
-create index idx_locations_iata_code
-	on locations (iata_code);
-
-create table measurement_results
-(
-    id integer
-        primary key,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
-    probe_id integer,
-    measurement_id integer,
-    measurement_timestamp integer,
-    measurement_date text,
-    time_average real,
-    time_max real,
-    time_min real,
-    ip text
-);
-
-create index idx_measurement_results_deleted_at
-	on measurement_results (deleted_at);
-
-create index idx_measurement_results_ip
-	on measurement_results (ip);
-
-create index idx_measurement_results_measurement_id
-	on measurement_results (measurement_id);
-
-create index idx_measurement_results_measurement_timestamp
-	on measurement_results (measurement_timestamp);
-
-create table measurements
-(
-    id integer
-        primary key,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
-    is_one_off numeric,
-    measurement_id integer,
-    start_time integer,
-    stop_time integer
-);
-
-create index idx_measurements_deleted_at
-	on measurements (deleted_at);
-
-create table migrations
-(
-    id VARCHAR(255)
-        primary key
-);
-
-create table miners
-(
-    id integer
-        primary key,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
-    address text,
-    ip text,
-    latitude real,
-    longitude real
-);
-
-create unique index idx_miners_address
-	on miners (address);
-
-create index idx_miners_deleted_at
-	on miners (deleted_at);
-
-create table probes
-(
-    id integer
-        primary key,
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
-    probe_id integer,
-    country_code text,
-    latitude real,
-    longitude real
-);
-
-create index idx_probes_deleted_at
-	on probes (deleted_at);
-
-create unique index idx_probes_probe_id
-	on probes (probe_id);
-
-create table sqlite_master
-(
-    type text,
-    name text,
-    tbl_name text,
-    rootpage int,
-    sql text
-);
+CREATE TABLE `miners` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`address` text,`ip` text,`latitude` real,`longitude` real,PRIMARY KEY (`id`));
+CREATE UNIQUE INDEX `idx_miners_address` ON `miners`(`address`);
+CREATE INDEX `idx_miners_deleted_at` ON `miners`(`deleted_at`);
+CREATE TABLE `locations` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`country` text,`iata_code` text,`latitude` real,`longitude` real,`type` text,PRIMARY KEY (`id`));
+CREATE UNIQUE INDEX `idx_locations_iata_code` ON `locations`(`iata_code`);
+CREATE INDEX `idx_locations_deleted_at` ON `locations`(`deleted_at`);
+CREATE TABLE `measurements` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`is_one_off` numeric,`measurement_id` integer,`start_time` integer,`stop_time` integer,PRIMARY KEY (`id`));
+CREATE INDEX `idx_measurements_deleted_at` ON `measurements`(`deleted_at`);
+CREATE TABLE `probes` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`probe_id` integer,`country_code` text,`iata_code` text,`latitude` real,`longitude` real,PRIMARY KEY (`id`),CONSTRAINT `fk_probes_location` FOREIGN KEY (`iata_code`) REFERENCES `locations`(`iata_code`));
+CREATE INDEX `idx_probes_deleted_at` ON `probes`(`deleted_at`);
+CREATE UNIQUE INDEX `idx_probes_probe_id` ON `probes`(`probe_id`);
+CREATE TABLE `measurement_results` (`id` integer,`created_at` datetime,`updated_at` datetime,`deleted_at` datetime,`probe_id` integer,`measurement_id` integer,`measurement_timestamp` integer,`ip` text,`measurement_date` text,`time_average` real,`time_max` real,`time_min` real,PRIMARY KEY (`id`),CONSTRAINT `fk_measurement_results_probe` FOREIGN KEY (`probe_id`) REFERENCES `probes`(`probe_id`),CONSTRAINT `fk_measurement_results_measurement` FOREIGN KEY (`measurement_id`) REFERENCES `measurements`(`measurement_id`));
+CREATE INDEX `idx_measurement_results_ip` ON `measurement_results`(`ip`);
+CREATE INDEX `idx_measurement_results_measurement_timestamp` ON `measurement_results`(`measurement_timestamp`);
+CREATE INDEX `idx_measurement_results_measurement_id` ON `measurement_results`(`measurement_id`);
+CREATE INDEX `idx_measurement_results_deleted_at` ON `measurement_results`(`deleted_at`);
+CREATE TABLE migrations (id VARCHAR(255) PRIMARY KEY);
