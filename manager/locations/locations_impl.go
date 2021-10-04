91b7fc45a8ab16c77da2dd6347891a3d95f13d12
Raphael Pralat <raphael.pralat@consensys.net>
2021-10-04 11:19:59 +0200

    fix: lint

--- manager/locations/locations_impl.go
+++ manager/locations/locations_impl.go
@@ -118,7 +118,7 @@ func (srv *LocationServiceImpl) UpdateLocations(airportType, filename string) er
 				lat, _ := strconv.ParseFloat(coords[0], 64)
 				long, _ := strconv.ParseFloat(coords[1], 64)
 				srv.DBMgr.GetDB().Create(&models.Location{
-					Name:   	 airport.Name,
+					Name:      airport.Name,
 					Country:   airport.IsoCountry,
 					IataCode:  airport.IataCode,
 					Latitude:  lat,
