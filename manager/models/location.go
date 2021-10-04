83725fedc444109f921a8964f8b22745de6558f1
Nelson Lourenco <nelson.lourenco@consensys.net>
2021-10-04 14:25:27 +0100

    update json tags

--- manager/models/location.go
+++ manager/models/location.go
@@ -6,10 +6,10 @@ import (
 
 type Location struct {
 	gorm.Model `json:"-"`
-	Name       string
-	Country    string
-	IataCode   string  `gorm:"uniqueIndex"`
+	Name       string  `json:"name"`
+	Country    string  `json:"country"`
+	IataCode   string  `gorm:"uniqueIndex" json:"iata_code"`
 	Latitude   float64 `gorm:"column:latitude" json:"latitude,omitempty"`
 	Longitude  float64 `gorm:"column:longitude" json:"longitude,omitempty"`
-	Type       string
+	Type       string  `json:"type"`
 }
--- manager/models/probes.go
+++ manager/models/probes.go
@@ -6,9 +6,9 @@ import (
 
 type Probe struct {
 	gorm.Model  `json:"-"`
-	ProbeID     int      `gorm:"column:probe_id;uniqueIndex"`
-	CountryCode string   `gorm:"column:country_code"`
-	IataCode    string   `gorm:"foreignKey:iata_code"`
+	ProbeID     int      `gorm:"column:probe_id;uniqueIndex" json:"probe_id"`
+	CountryCode string   `gorm:"column:country_code" json:"country_code"`
+	IataCode    string   `gorm:"foreignKey:iata_code" json:"iata_code"`
 	Location    Location `gorm:"foreignkey:IataCode;references:iata_code" json:"-"`
 	Latitude    float64  `gorm:"column:latitude" json:"latitude,omitempty"`
 	Longitude   float64  `gorm:"column:longitude" json:"longitude,omitempty"`
