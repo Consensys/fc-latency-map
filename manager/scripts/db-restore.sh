255b219929de3de3b3d1b03e7e60b4c90d352805
Raphael Pralat <raphael.pralat@consensys.net>
2021-10-15 10:20:59 +0200

    feat: add -rm

--- manager/scripts/db-restore.sh
+++ manager/scripts/db-restore.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run --user $(id -u):$(id -g) -v /$(pwd)/data:/latency-db keinos/sqlite3 sh -c "sqlite3 /latency-db/database.db < /latency-db/dump.sql"
+docker run --rm \
+  --user $(id -u):$(id -g) \
+  -v /$(pwd)/data:/latency-db keinos/sqlite3 \
+  sh -c "sqlite3 /latency-db/database.db < /latency-db/dump.sql"
